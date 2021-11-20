0bd760ec1bf21ce700b297cbeb22f3921cc823a3
Omar Aguirre <omar.fi.wwr@gmail.com>
2021-11-19 22:12:43 -0600

    Funcionamiento de las llamadas

--- SurviveThePandemic/Assets/Scripts/Llamadas/Llamadas.cs
+++ SurviveThePandemic/Assets/Scripts/Llamadas/Llamadas.cs
@@ -8,30 +8,81 @@ using TMPro;
 
 public class Llamadas : MonoBehaviour
 {
+    //private bool callAnswer = false;
+    private bool callSound = false;
 
-    [Header("Configuracion objetivos del tutorial")]
-    public GameObject Phone;
+    [Header("Notificaciones")]
+    public GameObject Notificacion;
+    public TextMeshProUGUI txtNotificacion; 
+    public string textoNotificacion = "Presiona X para contestar";  
+
+    [Header("Llamadas")]
+    public AudioSource Phone;
     public GameObject interfaceLlamadas;
     public TextMeshProUGUI contenedorTexto;
-    public KeyCode answer =  KeyCode.X; 
-    public KeyCode keyNext =  KeyCode.Space; 
+    //public KeyCode KeyAnswer = KeyCode.X; 
+    //public KeyCode keyNext = KeyCode.Space; 
     public Llamada[] llamadas;
+
+    [Header("Configuracion de teclado")]
+    public ConfigDialogos configuracion;    
 
     // Start is called before the first frame update
     void Start()
     {
         interfaceLlamadas.SetActive(false);  
+        Notificacion.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Objetivos.singleton.finishTutorial){
-            if( Input.GetKey(answer) ){
-                interfaceLlamadas.SetActive(true);                  
+            if(callSound == false){                
+                StartCoroutine( StartCall() );       
             }
-        }      
+            else if( Input.GetKey(configuracion.ContestarLlamada) ){                
+                AnswerCall();  
+                //callAnswer = true;              
+            }
+        }              
     }
+
+    public IEnumerator StartCall(){
+        Phone.Play(0);
+        yield return new WaitForSeconds(2);
+        callSound = true;
+        Debug.Log("Ring Ring");
+        txtNotificacion.text = textoNotificacion;
+        Notificacion.SetActive(true);
+        //yield return new WaitForSeconds(2);        
+    }
+
+    public void AnswerCall(){
+        interfaceLlamadas.SetActive(true);    
+        Phone.Pause();
+        callSound = true;
+        Notificacion.SetActive(false);
+        StartCoroutine( SimularDialogos(llamadas[0].dialogos) );           
+    }
+
+    public IEnumerator SimularDialogos(Dialogo[] dialogos){
+        for( int i = 0; i < dialogos.Length; i++){
+            contenedorTexto.text = "";
+            for(int j = 0; j < dialogos[i].text.Length + 1; j++){
+                yield return new WaitForSeconds(configuracion.tiempoLetra);
+                if(Input.GetKey(configuracion.teclaSkip) || Input.GetKey(configuracion.teclaSkip2)){
+                    j = dialogos[i].text.Length; 
+                }
+                contenedorTexto.text = dialogos[i].talker + ": " + dialogos[i].text.Substring(0, j);                
+            }
+
+            yield return new WaitForSeconds(0.5f);
+            yield return new WaitUntil( () => Input.GetKeyUp(configuracion.teclaSiguienteFrase) );
+        }
+        interfaceLlamadas.SetActive(false);         
+    }    
+
 }
 
 [System.Serializable]
