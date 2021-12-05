06bc2245f23c92a284d74c8ae0688d3b1d8009d2
CarlosCorona <carloserickcorona11@gmail.com>
2021-12-05 13:18:29 -0600

    se comento el codigo con errores y se agrego un ; en la linea 7

--- SurviveThePandemic/Assets/Scripts/Dialogs/Tutorial2/DialogIU.cs
+++ SurviveThePandemic/Assets/Scripts/Dialogs/Tutorial2/DialogIU.cs
@@ -4,57 +4,58 @@ using UnityEngine;
 
 public class DialogIU : MonoBehaviour
 {
-   public Conversacion conversacion
-   [SerializableField]
-        private float textSpeed = 10;
+    //public Conversacion conversacion;
+    
+    //[SerializableField]
+    //private float textSpeed = 10;
 
-    [SerializableField]
-    private GameObject convContainer;
-    [SerializableField]
-    private GameObject pregContainer;
+    //[SerializableField]
+    //private GameObject convContainer;
+    //[SerializableField]
+    //private GameObject pregContainer;
 
-    [SerializableField]
-    private Image speakIm;
-    [SerializableField]
-    private textMeshProUGUI nombre;
-    [SerializableField]
-    private textMeshProUGUI convText;
+    //[SerializableField]
+    //private Image speakIm;
+    //[SerializableField]
+    //private textMeshProUGUI nombre;
+    //[SerializableField]
+    //private textMeshProUGUI convText;
 
-    [SerializableField]
-    private Button continuarButton;
-    [SerializableField]
-    private Button anteriorButton;
+    //[SerializableField]
+    //private Button continuarButton;
+    //[SerializableField]
+    //private Button anteriorButton;
 
-    public int localIn = 1;
+    //public int localIn = 1;
 
-    private void Start() {
-        convContainer.SetActive(true);
-        pregContainer.SetActive(false);
+    //private void Start() {
+    //    convContainer.SetActive(true);
+    //    pregContainer.SetActive(false);
 
-        siguienteButton.gameObjectSetActive(true);
-        anteriorButton.gameObjectSetActive(false);
-    }
+    //    siguienteButton.gameObjectSetActive(true);
+    //    anteriorButton.gameObjectSetActive(false);
+    //}
 
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            ActualizarTextos(1);
+    //public void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Q))
+    //    {
+    //        ActualizarTextos(1);
 
-        }
+    //    }
 
-    }
+    //}
 
-    public void ActualizarTextos(int comportamiento)
-    {
-        convContainer(true);
-        switch (comportamiento)
-        {
-            case -1:
-                if (localIn > 0)
-                {
-                    print("Dialogo anterior");
-                }
-        }
-    }
+    //public void ActualizarTextos(int comportamiento)
+    //{
+    //    convContainer(true);
+    //    switch (comportamiento)
+    //    {
+    //        case -1:
+    //            if (localIn > 0)
+    //            {
+    //                print("Dialogo anterior");
+    //            }
+    //    }
+    //}
 }
