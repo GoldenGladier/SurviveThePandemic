3a2576439896a35f6445e6908256f93a2806e29a
Omar Aguirre <omar.fi.wwr@gmail.com>
2021-12-07 03:26:47 -0600

    Se agregarón mejoras a barra de contagio

--- SurviveThePandemic/Assets/Scripts/VidaPlayer.cs
+++ SurviveThePandemic/Assets/Scripts/VidaPlayer.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// TextMeshPro
+using TMPro;
+
 public class VidaPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -13,14 +16,14 @@ public class VidaPlayer : MonoBehaviour
     public float vida = 100;
 
     public Image barraDeVida;
+    public TextMeshProUGUI nContagioText;
 
     // Update is called once per frame
     void Update()
     {
         vida = Mathf.Clamp(vida, 0, 100); //Minimo y Maximo de Vida
-
         barraDeVida.fillAmount = vida / 100; 
-
-
+        nContagioText.text = (100-vida) + "%";
+        // nContagioText.text = (100-vida) + "%/100%";
     }
 }
